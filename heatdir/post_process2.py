6e9c7d70d2f3a75def4540a0f26cfc3a25260408
Gaurav Gokhale <gaurav.gokhale@tum.de>
2023-05-05 00:02:10 +0200

    Update heatdir/post_process2.py

--- heatdir/post_process2.py
+++ heatdir/post_process2.py
@@ -7,6 +7,37 @@ resolution = np.array([500, 1500, 2500, 3500, 4500])
 file1 = open('log_graph_1.txt', 'r')
 lines = file1.readlines()
 
+# flags =
+#0 %        ["CFLAGS = -O1 -qopt-report-annotate -qopt-report-phase=vec,loop"     
+#1 %         "CFLAGS = -O2 -qopt-report-annotate -qopt-report-phase=vec,loop"
+#2 %         "CFLAGS = -O3 -qopt-report-annotate -qopt-report-phase=vec,loop"
+#3 %         "CFLAGS = -Ofast -qopt-report-annotate -qopt-report-phase=vec,loop"
+#4 %         "CFLAGS = -O2 -xhost -qopt-report-annotate -qopt-report-phase=vec,loop"
+#5 %         "CFLAGS = -O2 -xCORE-AVX512 -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+#6          "CFLAGS = -O2 -xCORE-AVX512 -qopt-zmm-usage=high -ipo -qopt-report-annotate -qopt-report-phase=vec,loop"
+#7 %         "CFLAGS = -O2 -ipo -qopt-report-annotate -qopt-report-phase=vec,loop"
+#8 %         "CFLAGS = -O2 -xCORE-AVX512 -fno-alias -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 9         "CFLAGS = -O2 -xCORE-AVX512 -fno-alias -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 10 %       "CFLAGS = -O2 -fno-alias -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 11        "CFLAGS = -O3 -xhost -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 12        "CFLAGS = -O3 -xCORE-AVX512 -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 13        "CFLAGS = -O3 -xCORE-AVX512 -ipo -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 14        "CFLAGS = -O3 -ipo -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 15 %       "CFLAGS = -O3 -xCORE-AVX512 -fno-alias -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 16        "CFLAGS = -O3 -xCORE-AVX512 -fno-alias -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 17        "CFLAGS = -O3 -fno-alias -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 18        "CFLAGS = -Ofast -xhost -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 19        "CFLAGS = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 20        "CFLAGS = -Ofast -xCORE-AVX512 -ipo -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 21        "CFLAGS = -Ofast -ipo -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 22        "CFLAGS = -Ofast -xCORE-AVX512 -fno-alias -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 23 %       "CFLAGS = -Ofast -xCORE-AVX512 -fno-alias -qopt-zmm-usage=high -qopt-report-annotate -qopt-report-phase=vec,loop"
+# 24        "CFLAGS = -Ofast -fno-alias -qopt-report-annotate -qopt-report-phase=vec,loop"]
+
+idx_to_keep = [0,1,2,3,4,5,7,8,10,15,23]
+
+tags_kept = ['-O1','-O2','-O3','-Ofast','-O2 -xhost','-O2 -xCORE-AVX512','-O2 -ipo','-O2 -xCORE-AVX512 -fno-alias','-O2 -fno-alias','-O3 -xCORE-AVX512 -fno-alias','-Ofast -xCORE-AVX512 -fno-alias']
+
 count = 0
 tags = []
 res1 = []
@@ -14,6 +45,7 @@ res2 = []
 res3 = []
 res4 = []
 res5 = []
+
 while(count < len(lines)):
     res1.append(float(lines[count+1].split(';')[2]))
     res2.append(float(lines[count+2].split(';')[2]))
@@ -23,34 +55,14 @@ while(count < len(lines)):
     tags.append(lines[count])
     count += 7
 
-less = []
-mid = []
-high = []
-for i in range(0,len(res5)):
-    if(res5[i] < 2000):
-        less.append(i)
-    if(res5[i] > 2000 and res5[i] < 3000):
-        mid.append(i)
-    if(res5[i] > 3000):
-        high.append(i)
-
-print('**************************LOW**********************************')
-for idx in less:
-    print(tags[idx])
-    
-print('***************************MID*********************************')
-for idx in mid:
-    print(tags[idx])
-    
-print('****************************HIGH********************************')
-for idx in high:
-    print(tags[idx])
-    
 MFlops = np.stack((res1,res2,res3,res4,res5))
 lst = np.arange(1,len(tags))
 
-for i in lst:
-    plt.plot(resolution, MFlops[:,i-1], label = lst[i-1])
-    plt.legend(loc = "upper right", fontsize = "5")
-
+plt.figure(figsize=(30,15))
+plt.xlabel('Resolution', fontsize = "10")
+plt.ylabel('MFlop/s', fontsize = "10")
+plt.title('Group 6 initial performance data vs different compiler flags', fontsize = "10")
+for i,tag in enumerate(tags_kept):
+    plt.plot(resolution, MFlops[:,i], label = tag)
+    plt.legend(loc = "upper right", fontsize = "10")
 plt.show()
