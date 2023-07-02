93ed24cacb5ffff712d3c3a39dd09d421840304e
Durganshu Mishra <durganshu.mishra@tum.de>
2023-07-02 21:12:21 +0200

    Added new results for assignment3

--- assignment3/relax_jacobi.c
+++ assignment3/relax_jacobi.c
@@ -7,45 +7,38 @@
 
 #include "heat.h"
 
-/*
- * Residual (length of error vector)
- * between current solution and next after a Jacobi step
- */
-double residual_jacobi(double *u, unsigned sizex, unsigned sizey) {
-	unsigned i, j;
-	double unew, diff, sum = 0.0;
 
-	for (i = 1; i < sizey - 1; i++) {
-		for (j = 1; j < sizex - 1; j++) {
-		
-			unew = 0.25 * (u[i * sizex + (j - 1)] +  // left
-						u[i * sizex + (j + 1)] +  // right
-						u[(i - 1) * sizex + j] +  // top
-						u[(i + 1) * sizex + j]); // bottom
+double relax_jacobi( double **u1, double **utmp1,
+         unsigned sizex, unsigned sizey )
+{
+  int i, j;
+  double *help,*u, *utmp,factor=0.5;
 
-			diff = unew - u[i * sizex + j];
-			sum += diff * diff;
-		}
-	}
+  utmp=*utmp1;
+  u=*u1;
+  double unew, diff, sum=0.0;
 
-	return sum;
+
+  for( i=1; i<sizey-1; i++ ) {
+  	int ii=i*sizex;
+  	int iim1=(i-1)*sizex;
+  	int iip1=(i+1)*sizex;
+#pragma ivdep
+    for( j=1; j<sizex-1; j++ ){
+       unew = 0.25 * (u[ ii+(j-1) ]+
+        		            u[ ii+(j+1) ]+
+        		            u[ iim1+j ]+
+        		            u[ iip1+j ]);
+		    diff = unew - u[ii + j];
+		    utmp[ii+j] = unew;
+		    sum += diff * diff;
+
+       }
+    }
+
+  *u1=utmp;
+  *utmp1=u;
+  return(sum);
 }
 
-/*
- * One Jacobi iteration step
- */
-double relax_jacobi( double **u, double **utmp, unsigned sizex, unsigned sizey ){
-	double diff, sum = 0.0;
-	double *temp = *u;
-	for (int i = 1; i < sizey - 1; i++) {
-		for (int j = 1; j < sizex - 1; j++) {
-			(*utmp)[i * sizex + j] = 0.25 * ((*u)[i * sizex + (j - 1)] +  // left
-						(*u)[i * sizex + (j + 1)] +  // right
-						(*u)[(i - 1) * sizex + j] +  // top
-						(*u)[(i + 1) * sizex + j]); // bottom
 
-		}
-	}
-	*u = *utmp;
-  	*utmp = temp;
-}
